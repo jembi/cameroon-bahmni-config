3374039327262b9a61c5fc125a89428cf27540d6
Tresor <tresor.mvumbi@jembi.org>
2019-04-05 15:44:57 +0200

    COM-171 Fix travis script

--- scripts/install-report.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-cd ~/git/cameroon-bahmni-config
-git reset --hard
-git checkout COM-171
-git pull
-mysql -u root -ppassword openmrs < openmrs/apps/reports/PECG_REPORT/sql/report_util_functions.sql
-mysql -u root -ppassword openmrs < openmrs/apps/reports/PECG_REPORT/sql/indicator_functions.sql
