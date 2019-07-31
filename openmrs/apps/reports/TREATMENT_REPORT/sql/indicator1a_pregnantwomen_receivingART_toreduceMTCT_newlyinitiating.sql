3c1c461114905f7769ec581a90dedef91f8af27a
IipperMangesh <mangeshi@satvix.com>
2019-07-31 11:21:44 +0530

    COM-79 Added treatment indicator display part in report using sql and give its ref in reports.json file

---
+++ openmrs/apps/reports/TREATMENT_REPORT/sql/indicator1a_pregnantwomen_receivingART_toreduceMTCT_newlyinitiating.sql
@@ -0,0 +1,12 @@
+SELECT 'Number of HIV-positive pregnant women who received ART to reduce the risk of mother-to-child-transmission during pregnancy' AS '-', 'New on ART'  AS '-',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 0, 10, 0, '') AS '<10 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 10, 14, 1, '') AS '10-14 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 15, 19, 1, '') AS '15-19 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 20, 24, 1, '') AS '20-24 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 25, 29, 1, '') AS '25-29 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 30, 34, 1, '') AS '30-34 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 35, 39, 1, '') AS '35-39 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 40, 44, 1, '') AS '40-44 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 45, 49, 1, '') AS '45-49 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 50, 200, 1, '') AS '>=50 years',
+            TREATMENT_Indicator1a('#startDate#','#endDate#', 0, 0, 0, '') AS 'Unknown Age';
