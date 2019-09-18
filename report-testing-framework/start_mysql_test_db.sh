c97c0130eef2e050e69c47093b8d965317642d8d
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2019-09-18 15:56:54 +0200

    COM-38 Fix travis script

--- report-testing-framework/start_mysql_test_db.sh
+++ report-testing-framework/start_mysql_test_db.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 docker-compose up -d
 sleep 60
-docker exec -i bahmni_report_test_db mysql -uroot -pdbtest  <<< "UPDATE mysql.user SET host = '%' WHERE user = 'root';"
+docker exec -i bahmni_report_test_db mysql -uroot -pdbtest  <<EOF
+UPDATE mysql.user SET host = '%' WHERE user = 'root';
+EOF
 docker restart bahmni_report_test_db
