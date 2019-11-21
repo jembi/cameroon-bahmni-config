e7c059d6cf9175f97a50c4b41bd79c18094a95a5
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2019-11-21 16:38:20 +0200

    COM-89 Implement testing indicator 3a Implements testing indicator 3a

---
+++ openmrs/apps/reports/TESTING_REPORT/sql/indicator3a_hiv_positive_by_12_months.sql
@@ -0,0 +1,3 @@
+SELECT 'Positive' AS 'Number of HIV-infected infants identified in the reporting period, whose diagnostic sample was collected by 12 months of age',
+    Testing_Indicator3a('#startDate#','#endDate#', 0, 2, 1) AS '0<=2',
+    Testing_Indicator3a('#startDate#','#endDate#', 2, 12, 0) AS '2-12';
