b4a3d0c99159958d9894c37b87ffddddcc2b2e99
lauravignoli <vignoli.laura@gmail.com>
2020-01-15 14:47:03 +0200

    COM-599 Update testing report template

---
+++ openmrs/apps/reports/TESTING_REPORT/sql/indicator1c_not_available.sql
@@ -0,0 +1,9 @@
+SELECT 'Not Available' AS 'Title',
+    0 AS '<1 years M Pos',
+    0 AS '<1 years F Pos',
+    0 AS '1 - 4 years M Pos',
+    0 AS '1 - 4 years F Pos',   
+    0 AS '<1 years M Neg',
+    0 AS '<1 years F Neg',
+    0 AS '1 - 4 years M Neg',
+    0 AS '1 - 4 years F Neg';
