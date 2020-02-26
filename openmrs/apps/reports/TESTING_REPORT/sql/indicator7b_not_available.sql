092861a2a37af8513644931ac19ae405547e185d
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2020-02-26 15:48:55 +0200

    COM-771 Add Indicator 7 to testing report template Updates the template of the testing report and adds Indicator 7

---
+++ openmrs/apps/reports/TESTING_REPORT/sql/indicator7b_not_available.sql
@@ -0,0 +1,12 @@
+SELECT 'Not available' AS 'Not available',
+    0 AS '<10 years',
+    0 AS '10-14 years',
+    0 AS '15-19 years',
+    0 AS '20-24 years',
+    0 AS '25-29 years',
+    0 AS '30-34 years',
+    0 AS '35-39 years',
+    0 AS '40-44 years',
+    0 AS '45-49 years',
+    0 AS '>=50 years',
+    0 AS 'Unknown Age';
