349952bf9e2dba196ada9f51a45294262872565a
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2020-03-06 10:43:42 +0200

    COM-786 Implement util functions for data completeness check report Adds the util functions to calculate all the columns in data completeness check report

--- openmrs/apps/reports/HIV_COMPLETENESS_CHECK/sql/hivCompletenessCheck.sql
+++ openmrs/apps/reports/HIV_COMPLETENESS_CHECK/sql/hivCompletenessCheck.sql
@@ -1,5 +1,5 @@
 SELECT
-	(@row_number:=@row_number + 1) as "Nr.",
+    (@row_number:=@row_number + 1) as "Nr.",
     pn.given_name as "First Name",
     pn.family_name as "Last Name",
     UPPER(per.gender) as "Gender",
@@ -10,50 +10,34 @@ SELECT
     patientHasAtLeastOneArvDrugPrescribed(pat.patient_id) as "Has at least one ARV drug prescribed?",
     patientLatestArvDrugWasDispensed(pat.patient_id) as "Latest ARV drug was dispensed?",
     patientTherapeuticLineSpecified(pat.patient_id) as "Therapeutic Line Specified?",
-    0 as "ART Appointment Scheduled?",
-    0 as "ART Dispensary Appointment Scheduled?",
-    0 as "Testing Entry Point and Modality Specified?",
-    0 as "Informed Consent Specified?",
-    0 as "HIV Test Date Specified?",
-    0 as "Final Test Result Specified?",
-    0 as "Index Testing offered?",
-    0 as "Index Testing Accepted?",
-    0 as "Viral Load Exam Done?",
-    0 as "CD4 Exam Done?",
-    0 as "Nr. Of ART Visits",
-    0 as "Nr. Of ART Dispensary Visits",
-    0 as "Date of Last ART or ART Dispensary Visit",
-    0 as "Patient with HIV - Initial Form - Date Last Modified",
-    0 as "Patient with HIV - Follow Up Form - Date Last Modified",
-    0 as "Manual Lab Results Form - Date Last Modified",
-    0 as "Child Exposed to HIV Form - Date Last Modified",
-    0 as "Child Exposed to HIV Follow Up Form - Date Last Modified",
-    0 as "ANC Initial Form - Date Last Modified",
-    0 as "ANC Follow Up Form - Date Last Modified",
-    0 as "HIV Testing and Counselling Form - Date Last Modified",
-    0 as "History and Examination Form - Date Last Modified",
-    0 as "System and Physical Exams Form - Date Last Modified"
+    patientHasScheduledAnAppointmentDuringReportingPeriod(pat.patient_id,"#startDate#","#endDate#","APPOINTMENT_SERVICE_ART_KEY") as "ART Appointment Scheduled?",
+    patientHasScheduledAnAppointmentDuringReportingPeriod(pat.patient_id,"#startDate#","#endDate#","APPOINTMENT_SERVICE_ART_DISPENSARY_KEY") as "ART Dispensary Appointment Scheduled?",
+    getPatientEntryPointAndModality(pat.patient_id) as "Testing Entry Point and Modality Specified?",
+    getPatientInformedConsent(pat.patient_id) as "Informed Consent Specified?",
+    getPatientHIVTestDate(pat.patient_id) as "HIV Test Date Specified?",
+    getPatientHIVFinalTestResult(pat.patient_id) as "Final Test Result Specified?",
+    getPatientIndexTestingOffered(pat.patient_id) as "Index Testing offered?",
+    getPatientIndexTestingAccepted(pat.patient_id) as "Index Testing Accepted?",
+    patientHadALeastOneViralLoadExam(pat.patient_id) as "Viral Load Exam Done?",
+    patientHadACD4Exam(pat.patient_id) as "CD4 Exam Done?",
+    getNumberOfScheduledAppointmentsDuringReportingPeriod(pat.patient_id,"#startDate#","#endDate#","APPOINTMENT_SERVICE_ART_KEY") as "Nr. Of ART Visits",
+    getNumberOfScheduledAppointmentsDuringReportingPeriod(pat.patient_id,"#startDate#","#endDate#","APPOINTMENT_SERVICE_ART_DISPENSARY_KEY") as "Nr. Of ART Dispensary Visits",
+    getDateOfLastScheduledARTOrARTDispensaryAppointment(pat.patient_id) as "Date of Last ART or ART Dispensary Visit",
+    getPatientHivInitialFormLastModificationDate(pat.patient_id) as "Patient with HIV - Initial Form - Date Last Modified",
+    getPatientHivFollowUpFormLastModificationDate(pat.patient_id) as "Patient with HIV - Follow Up Form - Date Last Modified",
+    getPatientLabResultLastModificationDate(pat.patient_id) as "Manual Lab Results Form - Date Last Modified",
+    getPatientChildExposedLastModificationDate(pat.patient_id) as "Child Exposed to HIV Form - Date Last Modified",
+    getPatientChildExposedFollowUpLastModificationDate(pat.patient_id) as "Child Exposed to HIV Follow Up Form - Date Last Modified",
+    getPatientANCInitialLastModificationDate(pat.patient_id) as "ANC Initial Form - Date Last Modified",
+    getPatientANCFollowUpLastModificationDate(pat.patient_id) as "ANC Follow Up Form - Date Last Modified",
+    getPatientHIVTestingAndCounsellingLastModificationDate(pat.patient_id) as "HIV Testing and Counselling Form - Date Last Modified",
+    getPatientHistoryAndExaminationLastModificationDate(pat.patient_id) as "History and Examination Form - Date Last Modified",
+    getPatientSystemAndPhysicalExamLastModificationDate(pat.patient_id) as "System and Physical Exams Form - Date Last Modified"
 FROM patient pat
 	JOIN person_name pn ON pn.person_id = pat.patient_id
     JOIN person per ON per.person_id = pat.patient_id
     JOIN patient_identifier pi ON pi.patient_id = pat.patient_id,
     (SELECT @row_number:=0) AS t
 WHERE
-   (
-       patientHasRegisteredWithinReportingPeriod(pat.patient_id, "#startDate#","#endDate#") = TRUE AND
-       (
-            patientHasEnrolledIntoHivProgram(pat.patient_id) = "No" OR
-            arvInitiationDateSpecified(pat.patient_id) = "No" OR
-            patientHasAtLeastOneArvDrugPrescribed(pat.patient_id) = "No" OR
-            patientLatestArvDrugWasDispensed(pat.patient_id) = "No" OR
-            patientTherapeuticLineSpecified(pat.patient_id) = "No"
-       ) AND 
-       (
-            patientHasEnrolledIntoHivProgram(pat.patient_id) = "Yes" OR
-            arvInitiationDateSpecified(pat.patient_id) = "Yes" OR
-            patientHasAtLeastOneArvDrugPrescribed(pat.patient_id) = "Yes" OR
-            patientLatestArvDrugWasDispensed(pat.patient_id) = "Yes" OR
-            patientTherapeuticLineSpecified(pat.patient_id) = "Yes"
-       )
-   );
-	
+    pi.preferred = 1 AND
+    patientHadAHivRelatedVisitWithinReportingPeriod(pat.patient_id, "#startDate#","#endDate#");
