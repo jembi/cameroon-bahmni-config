f0d47cb369e3571d0f464577e70e67c5198076c5
Tresor Mvumbi <tresor.mvumbi@jembi.org>
2020-04-08 10:38:03 +0200

    COM-681 Add index indicator 1b Implements the index indicator 1b

--- metadata/reportssql/index_report_functions.sql
+++ metadata/reportssql/index_report_functions.sql
@@ -27,6 +27,35 @@ WHERE
 END$$ 
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS Index_Indicator1b;
+
+DELIMITER $$
+CREATE FUNCTION Index_Indicator1b(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_gender VARCHAR(1)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+    DECLARE uuidIndexTestingDateOffered VARCHAR(38) DEFAULT "836fe9d4-96f1-4fea-9ad8-35bd06e0ee05";
+
+SELECT
+    COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+    patient pat
+WHERE
+    patientGenderIs(pat.patient_id, p_gender) AND
+    patientAgeAtReportEndDateIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge, p_endDate) AND
+    getPatientIndexTestingDateOffered(pat.patient_id) IS NOT NULL AND
+    getObsLocation(pat.patient_id, uuidIndexTestingDateOffered) IN ("LOCATION_COMMUNITY_HOME", "LOCATION_COMMUNITY_MOBILE");
+
+    RETURN (result);
+END$$
+DELIMITER ;
+
 -- getObsLocation
 
 DROP FUNCTION IF EXISTS getObsLocation;
