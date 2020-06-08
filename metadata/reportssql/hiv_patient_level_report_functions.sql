6e0e65d88a2db289693e782f501ccec402aa5637
lauravignoli <vignoli.laura@gmail.com>
2020-06-08 16:07:05 +0200

    COM-841 Add radet relationship function

--- metadata/reportssql/hiv_patient_level_report_functions.sql
+++ metadata/reportssql/hiv_patient_level_report_functions.sql
@@ -11,3 +11,30 @@ BEGIN
     RETURN IF(gender = "m", "Male", "Female");
 END$$
 DELIMITER ;
+
+-- getRadetFirstIndexRelationship
+
+DROP FUNCTION IF EXISTS getRadetFirstIndexRelationship;
+
+DELIMITER $$
+CREATE FUNCTION getRadetFirstIndexRelationship(
+    p_patientId INT(11)) RETURNS VARCHAR(255)
+    DETERMINISTIC
+BEGIN
+    DECLARE relationship VARCHAR(50) DEFAULT getFirstIndexRelationship(p_patientId);
+
+    IF (relationship = "RELATIONSHIP_SPOUSE") THEN
+        RETURN "SPOUSE";
+    ELSEIF (relationship = "RELATIONSHIP_PARTNER") THEN
+        RETURN "Sexual contact (other than Spouse)";
+    ELSEIF (relationship = "RELATIONSHIP_BIO_CHILD") THEN
+        RETURN "Biological child of index case <15 years";
+    ELSEIF (relationship = "RELATIONSHIP_BIO_MOTHER" OR relationship = "RELATIONSHIP_BIO_FATHER") THEN
+        RETURN "Biological parent of an index child";
+    ELSEIF (relationship = "RELATIONSHIP_SIBLING") THEN
+        RETURN "Sibling of index case <15 years";
+    ELSE
+        RETURN "Other";
+    END IF;
+END$$
+DELIMITER ;
