6eef967f56816a90cb1aeac2a8cd06b54be6e923
Parth-Satvix <parthtrivedi.satvix@gmail.com>
2021-06-11 12:34:03 +0530

    Parth | Worked on card COM-1001

---
+++ odoo_custom_modules/store_balance_sheet_report/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import balance_sheet_report
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
