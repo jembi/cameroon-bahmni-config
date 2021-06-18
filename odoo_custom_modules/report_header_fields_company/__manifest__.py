fbcf07396d9f986077d7f3d9dedc978a8e50e1b3
Parth-Satvix <parthtrivedi.satvix@gmail.com>
2021-06-18 17:33:54 +0530

    [Mod]modify code to add country filed for report header in company form view

---
+++ odoo_custom_modules/report_header_fields_company/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Report Header Fields Company',
+    'version': '1.0',
+    'summary': 'Add report header related fields in company',
+    'sequence': 1,
+    'description': """
+Add report header related fields in company
+====================
+""",
+    'category': 'base',
+    'website': '',
+    'images': [],
+    'depends': ['base',],
+    'data': ['views/res_company.xml',
+             ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
