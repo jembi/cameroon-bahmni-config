1ff0633fc4c76c06eefc45705e12f5c81421a1e1
ningosi <nicotwendelee@gmail.com>
2022-10-26 16:35:22 +0300

    Adjust template to include under 1 year and put in a section of the function to filter the count of patient

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -3,7 +3,23 @@
 DROP FUNCTION IF EXISTS HIVTMI_Indicator1;
 
 DELIMITER $$
-CREATE FUNCTION HIVTMI_Indicator1()
+CREATE FUNCTION HIVTMI_Indicator1(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_gender VARCHAR(1)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
 
-  END$$
+SELECT
+    COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+    patient pat
+WHERE
+
+RETURN (result);
+END$$
 DELIMITER ;
