c89e4f32708a2c72290b44b77d9483ca0b39220d
ningosi <nicotwendelee@gmail.com>
2022-10-26 17:36:09 +0300

    Add the logic to find patients for indicator 1

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -19,6 +19,10 @@ SELECT
 FROM
     patient pat
 WHERE
+  patientGenderIs(pat.patient_id, p_gender) AND 
+  patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND 
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL;
 
 RETURN (result);
 END$$
