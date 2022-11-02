430d6905441048e89a266bed6635f993029af75e
tresor <tresorluz21@gmail.com>
2022-11-02 18:05:29 +0200

    COM-1188 added indicator 2 in Hiv report

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -27,3 +27,31 @@ WHERE
 RETURN (result);
 END$$
 DELIMITER ;
+
+DROP FUNCTION IF EXISTS HIVTMI_Indicator2;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator2(
+    p_startDate DATE,
+    p_endDate DATE,
+    p_startAge INT(11),
+    p_endAge INT (11),
+    p_includeEndAge TINYINT(1),
+    p_gender VARCHAR(1)) RETURNS INT(11)
+    DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+    COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+    patient pat
+WHERE
+  patientGenderIs(pat.patient_id, p_gender) AND 
+  patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
+  getObsCodedValue(pat.patient_id, "55d59198-1d83-45db-9e92-dc3b9af25ca6") = "Yes" AND 
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL;
+
+RETURN (result);
+END$$
+DELIMITER ;
