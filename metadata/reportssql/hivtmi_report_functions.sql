8f5e757a1a4e7bc75db8c6d3ecafcc32e4c57b7e
ningosi <nicotwendelee@gmail.com>
2022-11-03 16:44:34 +0300

    Add a single function that will take a string of possible entry points to comapre from

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -28,14 +28,14 @@ RETURN (result);
 END$$
 DELIMITER ;
 
--- Number of people tested in the month - desegregate by single entry point
-DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_single_entry_point;
+-- Number of people tested in the month - desegregate by entry point
+DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_entry_point;
 
 DELIMITER $$
-CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_single_entry_point(
+CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_entry_point(
   p_startDate DATE,
   p_endDate DATE,
-  disgCodedValue VARCHAR(256)) RETURNS INT(11)
+  responses VARCHAR(256)) RETURNS INT(11)
 DETERMINISTIC
 BEGIN
     DECLARE result INT(11) DEFAULT 0;
@@ -47,82 +47,8 @@ FROM
 WHERE
   getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
   getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
-  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") = disgCodedValue;
+  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN(responses);
 
-RETURN (result);
-END$$
-DELIMITER ;
-
--- Number of people tested in the month - desegregate by two entry point
-DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_two_entry_point;
-
-DELIMITER $$
-CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_two_entry_point(
-  p_startDate DATE,
-  p_endDate DATE,
-  disgCodedValue1 VARCHAR(256),
-  disgCodedValue2 VARCHAR(256)) RETURNS INT(11)
-DETERMINISTIC
-BEGIN
-    DECLARE result INT(11) DEFAULT 0;
-
-SELECT
-  COUNT(DISTINCT pat.patient_id) INTO result
-FROM
-  patient pat
-WHERE
-  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
-  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
-  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN(disgCodedValue1, disgCodedValue2);
-
-RETURN (result);
-END$$
-DELIMITER ;
-
--- Number of people tested in the month - desegregate by other entry point
-DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_other_entry_point;
-
-DELIMITER $$
-CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_other_entry_point(
-  p_startDate DATE,
-  p_endDate DATE
-  ) RETURNS INT(11)
-DETERMINISTIC
-BEGIN
-    DECLARE result INT(11) DEFAULT 0;
-
-SELECT
-  COUNT(DISTINCT pat.patient_id) INTO result
-FROM
-  patient pat
-WHERE
-  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
-  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
-  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN ("Malnutrition", "OPD", "Other PITC", "STI", "VMMC", "Laboratory", "Community Testing-Outreach", "Community Testing-Satellite Site", "Treatment Unit (UPEC)", "Partners of PW", "Partners of BFW", "PITC (Outpatient Department, casuality)");
-RETURN (result);
-END$$
-DELIMITER ;
-
--- Number of people tested in the month - desegregate by ANC entry point
-DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_anc_entry_point;
-
-DELIMITER $$
-CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_anc_entry_point(
-  p_startDate DATE,
-  p_endDate DATE
-) RETURNS INT(11)
-             DETERMINISTIC
-BEGIN
-    DECLARE result INT(11) DEFAULT 0;
-
-SELECT
-  COUNT(DISTINCT pat.patient_id) INTO result
-FROM
-  patient pat
-WHERE
-  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
-  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
-    getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN("PMTCT [ANC1-Only]", "PMTCT [Post ANC1]", "Other testing at ANC");
 RETURN (result);
 END$$
 DELIMITER ;
