f0f1ebc9844a7dce0ff12bb7a1077e126f29e312
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2022-11-08 14:00:24 +0300

    Com 1192 (#590)
    * Add implenentation for indicator 1 disaggregated by entry point
    
    * Add indicator 1 disaggregated with entry point
    
    * Add a single function that will take a string of possible entry points to comapre from
    
    * Map the first indicator for disaggregation by the entry point
    
    * Change the string value results for the indicator 2 from Yes to True
    
    * Supply the correct names for the concepts saved in the obs as coded value names

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -4,24 +4,24 @@ DROP FUNCTION IF EXISTS HIVTMI_Indicator1;
 
 DELIMITER $$
 CREATE FUNCTION HIVTMI_Indicator1(
-    p_startDate DATE,
-    p_endDate DATE,
-    p_startAge INT(11),
-    p_endAge INT (11),
-    p_includeEndAge TINYINT(1),
-    p_gender VARCHAR(1)) RETURNS INT(11)
-    DETERMINISTIC
+  p_startDate DATE,
+  p_endDate DATE,
+  p_startAge INT(11),
+  p_endAge INT (11),
+  p_includeEndAge TINYINT(1),
+  p_gender VARCHAR(1)) RETURNS INT(11)
+                                  DETERMINISTIC
 BEGIN
     DECLARE result INT(11) DEFAULT 0;
 
 SELECT
-    COUNT(DISTINCT pat.patient_id) INTO result
+  COUNT(DISTINCT pat.patient_id) INTO result
 FROM
-    patient pat
+  patient pat
 WHERE
-  patientGenderIs(pat.patient_id, p_gender) AND 
+  patientGenderIs(pat.patient_id, p_gender) AND
   patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
-  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND 
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
   getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL;
 
 RETURN (result);
@@ -32,25 +32,127 @@ DROP FUNCTION IF EXISTS HIVTMI_Indicator2;
 
 DELIMITER $$
 CREATE FUNCTION HIVTMI_Indicator2(
-    p_startDate DATE,
-    p_endDate DATE,
-    p_startAge INT(11),
-    p_endAge INT (11),
-    p_includeEndAge TINYINT(1),
-    p_gender VARCHAR(1)) RETURNS INT(11)
-    DETERMINISTIC
+  p_startDate DATE,
+  p_endDate DATE,
+  p_startAge INT(11),
+  p_endAge INT (11),
+  p_includeEndAge TINYINT(1),
+  p_gender VARCHAR(1)) RETURNS INT(11)
+                                  DETERMINISTIC
 BEGIN
     DECLARE result INT(11) DEFAULT 0;
 
 SELECT
-    COUNT(DISTINCT pat.patient_id) INTO result
+  COUNT(DISTINCT pat.patient_id) INTO result
 FROM
-    patient pat
+  patient pat
 WHERE
-  patientGenderIs(pat.patient_id, p_gender) AND 
+  patientGenderIs(pat.patient_id, p_gender) AND
   patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
-  getObsCodedValue(pat.patient_id, "55d59198-1d83-45db-9e92-dc3b9af25ca6") = "Yes" AND 
+  getObsCodedValue(pat.patient_id, "55d59198-1d83-45db-9e92-dc3b9af25ca6") = "True" AND
   getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL;
+
+RETURN (result);
+END$$
+DELIMITER ;
+
+-- Number of people tested in the month - desegregate by entry point
+DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_single_entry_point;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_single_entry_point(
+  p_startDate DATE,
+  p_endDate DATE,
+  response VARCHAR(256)) RETURNS INT(11)
+                                     DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+  COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+  patient pat
+WHERE
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
+  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") = response;
+
+RETURN (result);
+END$$
+DELIMITER ;
+
+
+
+DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_two_entry_point;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_two_entry_point(
+  p_startDate DATE,
+  p_endDate DATE,
+  response1 VARCHAR(256),
+  response2 VARCHAR(256)) RETURNS INT(11)
+  DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+  COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+  patient pat
+WHERE
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
+  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN(response1, response2);
+
+RETURN (result);
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_three_entry_point;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_three_entry_point(
+  p_startDate DATE,
+  p_endDate DATE,
+  response1 VARCHAR(256),
+  response2 VARCHAR(256),
+  response3 VARCHAR(256)) RETURNS INT(11)
+                                     DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+  COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+  patient pat
+WHERE
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
+    getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN(response1, response2, response2);
+
+RETURN (result);
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS HIVTMI_Indicator1_disaggregated_by_other_entry_points;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator1_disaggregated_by_other_entry_points(
+  p_startDate DATE,
+  p_endDate DATE
+  ) RETURNS INT(11)
+DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+  COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+  patient pat
+WHERE
+  getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
+    getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN("Malnutrition", "Operative Notes, OPD", "Other PITC", "STI", "VMMC", "LOCATION_LABORATORY", "Community Testing-Outreach", "Community Testing-Satellite Site", "Treatment Unit (UPEC)", "Partners of PW", "Partners of BFW", "PITC (Outpatient Department - casuality)");
 
 RETURN (result);
 END$$
