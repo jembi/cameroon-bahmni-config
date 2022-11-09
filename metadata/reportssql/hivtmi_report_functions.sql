18198b4112902d805283e28621ec376682490c7e
tresor <tresorluz21@gmail.com>
2022-11-09 14:30:39 +0200

    Added indicator5 to the Hiv report

--- metadata/reportssql/hivtmi_report_functions.sql
+++ metadata/reportssql/hivtmi_report_functions.sql
@@ -28,6 +28,7 @@ RETURN (result);
 END$$
 DELIMITER ;
 
+-- Number of people who withdrew their results in the month
 DROP FUNCTION IF EXISTS HIVTMI_Indicator2;
 
 DELIMITER $$
@@ -152,7 +153,35 @@ FROM
 WHERE
   getObsDatetimeValueInSection(pat.patient_id, "c6c08cdc-18dc-4f42-809c-959621bc9a6c", "b70dfca0-db21-4533-8c08-4626ff0de265") BETWEEN p_startDate AND p_endDate AND
   getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") IS NOT NULL AND
-    getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN("Malnutrition", "Operative Notes, OPD", "Other PITC", "STI", "VMMC", "LOCATION_LABORATORY", "Community Testing-Outreach", "Community Testing-Satellite Site", "Treatment Unit (UPEC)", "Partners of PW", "Partners of BFW", "PITC (Outpatient Department - casuality)");
+  getObsCodedValue(pat.patient_id, "bc43179d-00b4-4712-a5d6-4dabd4230888") IN("Malnutrition", "Operative Notes, OPD", "Other PITC", "STI", "VMMC", "LOCATION_LABORATORY", "Community Testing-Outreach", "Community Testing-Satellite Site", "Treatment Unit (UPEC)", "Partners of PW", "Partners of BFW", "PITC (Outpatient Department - casuality)");
+
+RETURN (result);
+END$$
+DELIMITER ;
+
+-- Number of HIV indeterminate results in the month
+DROP FUNCTION IF EXISTS HIVTMI_Indicator5;
+
+DELIMITER $$
+CREATE FUNCTION HIVTMI_Indicator5(
+  p_startDate DATE,
+  p_endDate DATE,
+  p_startAge INT(11),
+  p_endAge INT (11),
+  p_includeEndAge TINYINT(1),
+  p_gender VARCHAR(1)) RETURNS INT(11)
+                                  DETERMINISTIC
+BEGIN
+    DECLARE result INT(11) DEFAULT 0;
+
+SELECT
+  COUNT(DISTINCT pat.patient_id) INTO result
+FROM
+  patient pat
+WHERE
+  patientGenderIs(pat.patient_id, p_gender) AND
+  patientAgeWhenRegisteredForHivProgramIsBetween(pat.patient_id, p_startAge, p_endAge, p_includeEndAge) AND
+  getObsCodedValueInSectionByNames(pat.patient_id, "Final Test Result", "Final Result") = "Indeterminate";
 
 RETURN (result);
 END$$
