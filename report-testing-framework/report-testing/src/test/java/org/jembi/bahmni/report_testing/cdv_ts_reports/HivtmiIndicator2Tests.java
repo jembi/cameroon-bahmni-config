2539c9f73b2314b962b0fdf3d05a87b5d32d4072
tresor <tresorluz21@gmail.com>
2023-02-16 10:38:48 +0200

    COM-1215 Added fixes for hiv indicator2 unit tests

--- report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/cdv_ts_reports/HivtmiIndicator2Tests.java
+++ report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/cdv_ts_reports/HivtmiIndicator2Tests.java
@@ -47,7 +47,7 @@ public class HivtmiIndicator2Tests extends BaseReportTest {
             new LocalDate(2022, 11, 5),
             encounterId);
 
-        // set hiv test date
+        // set hiv result received to True
         testDataGenerator.hivTestingAndCounsellingForm.setHTCResultReceived(
             patientId,
             new LocalDateTime(2022, 11, 5, 8, 0),
@@ -196,6 +196,14 @@ public class HivtmiIndicator2Tests extends BaseReportTest {
             new LocalDateTime(2022, 11, 5, 8, 0),
             new LocalDate(2022, 11, 5),
             encounterId);
+
+        // set hiv result received to True
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCResultReceived(
+            patientId,
+            new LocalDateTime(2022, 11, 5, 8, 0),
+            ConceptEnum.TRUE,
+            encounterId);
+
         // set hiv final result
         testDataGenerator.hivTestingAndCounsellingForm.setHTCFinalResult(
             patientId,
@@ -237,6 +245,78 @@ public class HivtmiIndicator2Tests extends BaseReportTest {
         assertEquals(result.get(0).get(">=50 M"), 0);
         assertEquals(result.get(0).get(">=50 F"), 0);
 
+    }
+
+    @Test
+    public void shouldNotCountPatientWhoDidNotWithdrawHivResults() throws Exception {
+        // register a new patient
+        int patientId = testDataGenerator.registration.createPatient(
+            GenderEnum.MALE,
+            new LocalDate(2000, 9, 1),
+            "John",
+            "Doe");
+
+        // start a new visit
+        int encounterId = testDataGenerator.startVisit(
+            patientId,
+            new LocalDate(2022, 11, 1),
+            VisitTypeEnum.VISIT_TYPE_OPD);
+
+        // enrol patient into hiv program
+        testDataGenerator.program.enrollPatientIntoHIVProgram(
+            patientId,
+            new LocalDate(2022, 11, 3),
+            ConceptEnum.WHO_STAGE_1,
+            TherapeuticLineEnum.FIRST_LINE,
+            new LocalDate(2022, 11, 3));
+
+        // set hiv test date
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCHivTestDate(
+            patientId,
+            new LocalDateTime(2022, 11, 5, 8, 0),
+            new LocalDate(2022, 11, 5),
+            encounterId);
+
+        // set hiv final result
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCFinalResult(
+            patientId,
+            new LocalDateTime(2022, 11, 11, 8, 0),
+            ConceptEnum.NEGATIVE,
+            encounterId);
+
+        // execute
+        String query = readReportQuery(ReportEnum.CDV_TS_REPORT,
+            "indicator2_CDV_TS_number_of_people_who_withdrew_their_results_in_the_month.sql",
+            new LocalDate(2022, 11, 1),
+            new LocalDate(2022, 11, 30));
+        List<Map<String, Object>> result = getReportResult(query);
+
+        // Assert
+        assertEquals(result.get(0).get("Title"), "Number of people who withdrew their results in the month");
+        assertEquals(result.get(0).get("<1 M"), 0);
+        assertEquals(result.get(0).get("<1 F"), 0);
+        assertEquals(result.get(0).get("1-4 M"), 0);
+        assertEquals(result.get(0).get("1-4 F"), 0);
+        assertEquals(result.get(0).get("5-9 M"), 0);
+        assertEquals(result.get(0).get("5-9 F"), 0);
+        assertEquals(result.get(0).get("10-14 M"), 0);
+        assertEquals(result.get(0).get("10-14 F"), 0);
+        assertEquals(result.get(0).get("15-19 M"), 0);
+        assertEquals(result.get(0).get("15-19 F"), 0);
+        assertEquals(result.get(0).get("20-24 M"), 0);
+        assertEquals(result.get(0).get("20-24 F"), 0);
+        assertEquals(result.get(0).get("25-29 M"), 0);
+        assertEquals(result.get(0).get("25-29 F"), 0);
+        assertEquals(result.get(0).get("30-34 M"), 0);
+        assertEquals(result.get(0).get("30-34 F"), 0);
+        assertEquals(result.get(0).get("35-39 M"), 0);
+        assertEquals(result.get(0).get("35-39 F"), 0);
+        assertEquals(result.get(0).get("40-44 M"), 0);
+        assertEquals(result.get(0).get("40-44 F"), 0);
+        assertEquals(result.get(0).get("45-49 M"), 0);
+        assertEquals(result.get(0).get("45-49 F"), 0);
+        assertEquals(result.get(0).get(">=50 M"), 0);
+        assertEquals(result.get(0).get(">=50 F"), 0);
     }
     
 }
