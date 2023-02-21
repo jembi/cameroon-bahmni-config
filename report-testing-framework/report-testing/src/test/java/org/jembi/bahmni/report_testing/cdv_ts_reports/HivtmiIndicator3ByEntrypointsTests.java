d370c01501cc08516844d92fa29082a87cd43099
tresor <tresorluz21@gmail.com>
2023-02-21 14:42:27 +0200

    COM-1193 changed hiv test date concept

--- report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/cdv_ts_reports/HivtmiIndicator3ByEntrypointsTests.java
+++ report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/cdv_ts_reports/HivtmiIndicator3ByEntrypointsTests.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 import java.util.Map;
 
-
 import org.jembi.bahmni.report_testing.test_utils.BaseReportTest;
 import org.jembi.bahmni.report_testing.test_utils.models.ConceptEnum;
 import org.jembi.bahmni.report_testing.test_utils.models.TherapeuticLineEnum;
@@ -20,70 +19,196 @@ import org.junit.Test;
 public class HivtmiIndicator3ByEntrypointsTests extends BaseReportTest {
     @Test
     public void shouldCountHivPatientByEntryPoints() throws Exception {
-		int patientId = testDataGenerator.registration.createPatient(
-            GenderEnum.MALE,
-            new LocalDate(2000, 9, 1),
-            "John",
-            "Doe"
-        );
-		int encounterId = testDataGenerator.startVisit(
-            patientId,
-            new LocalDate(2022, 11, 1),
-            VisitTypeEnum.VISIT_TYPE_OPD
-		);
+        // register a new patient
+        int patientId = testDataGenerator.registration.createPatient(
+                GenderEnum.MALE,
+                new LocalDate(2000, 9, 1),
+                "John",
+                "Doe");
 
+        // start a new visit
+        int encounterId = testDataGenerator.startVisit(
+                patientId,
+                new LocalDate(2022, 11, 1),
+                VisitTypeEnum.VISIT_TYPE_OPD);
+
+        // enrol patient into hiv program
         testDataGenerator.program.enrollPatientIntoHIVProgram(
-			patientId,
-			new LocalDate(2022, 11, 3),
-			ConceptEnum.WHO_STAGE_1,
-			TherapeuticLineEnum.FIRST_LINE,
-			new LocalDate(2022, 11, 3)
-		);
+                patientId,
+                new LocalDate(2022, 11, 3),
+                ConceptEnum.WHO_STAGE_1,
+                TherapeuticLineEnum.FIRST_LINE,
+                new LocalDate(2022, 11, 3));
 
-        testDataGenerator.ancInitialForm.recordAtAncEnrolmentHivTestDateAndResult(
-			patientId,
-			new LocalDateTime(2022, 11, 3, 8, 0),
-			new LocalDate(2022, 11, 3),
-			ConceptEnum.HIV_TEST_DATE,
-			encounterId
-		);
+        // set hiv test date
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCHivTestDate(
+                patientId,
+                new LocalDateTime(2022, 11, 5, 8, 0),
+                new LocalDate(2022, 11, 5),
+                encounterId);
 
+        // set hiv final result
         testDataGenerator.hivTestingAndCounsellingForm.setHTCFinalResult(
-            patientId,
-			new LocalDateTime(2022, 11, 11, 8, 0),
-            ConceptEnum.POSITIVE,
-            encounterId
-        );
-        
+                patientId,
+                new LocalDateTime(2022, 11, 11, 8, 0),
+                ConceptEnum.POSITIVE,
+                encounterId);
+
+        // set entrypoints
         testDataGenerator.hivTestingAndCounsellingForm.recordTestingEntryPointAndModality(
-            patientId,
-			new LocalDateTime(2022, 11, 11, 8, 0),
-            ConceptEnum.BLOOD_BANK,
-            encounterId
-        );
+                patientId,
+                new LocalDateTime(2022, 11, 11, 8, 0),
+                ConceptEnum.BLOOD_BANK,
+                encounterId);
 
+        // execute
         String query = readReportQuery(ReportEnum.CDV_TS_REPORT,
-         "indicator3_CDV_TS_number_of_people_tested_positive_in_the_month_disaggregation_entry_point.sql",
-          new LocalDate(2022, 11, 1),
-          new LocalDate(2022, 11, 30)
-          );
+                "indicator3_CDV_TS_number_of_people_tested_positive_in_the_month_disaggregation_entry_point.sql",
+                new LocalDate(2022, 11, 1),
+                new LocalDate(2022, 11, 30));
 
-        List<Map<String,Object>> result = getReportResult(query);
+        List<Map<String, Object>> result = getReportResult(query);
 
         // Assert
-		assertEquals(result.get(0).get("Title"), "Number of people tested positive in the month disaggregated by entry point");
-		assertEquals(result.get(0).get("Index"), 0);
-		assertEquals(result.get(0).get("Emergency"), 0);
-		assertEquals(result.get(0).get("CPN / ANC"), 0);
-		assertEquals(result.get(0).get("SA/Maternité / Delivery room/Postpartumn"), 0);
-		assertEquals(result.get(0).get("VCT"), 0);
-		assertEquals(result.get(0).get("Hospitalization"), 0);
-		assertEquals(result.get(0).get("Pediatrics"), 0);
-		assertEquals(result.get(0).get("Other testing at TB Unit"), 0);
-		assertEquals(result.get(0).get("Blood Bank"), 1);
-		assertEquals(result.get(0).get("Screening Campaign"), 0);
-		assertEquals(result.get(0).get("Other entry Points"), 0);
+        assertEquals(result.get(0).get("Title"),
+                "Number of people tested positive in the month disaggregated by entry point");
+        assertEquals(result.get(0).get("Index"), 0);
+        assertEquals(result.get(0).get("Emergency"), 0);
+        assertEquals(result.get(0).get("CPN / ANC"), 0);
+        assertEquals(result.get(0).get("SA/Maternité / Delivery room/Postpartumn"), 0);
+        assertEquals(result.get(0).get("VCT"), 0);
+        assertEquals(result.get(0).get("Hospitalization"), 0);
+        assertEquals(result.get(0).get("Pediatrics"), 0);
+        assertEquals(result.get(0).get("Other testing at TB Unit"), 0);
+        assertEquals(result.get(0).get("Blood Bank"), 1);
+        assertEquals(result.get(0).get("Screening Campaign"), 0);
+        assertEquals(result.get(0).get("Other entry Points"), 0);
 
     }
-    
+
+    @Test
+    public void shouldNotCountHivPatientWhenNoEntryPointsIsSelected() throws Exception {
+        // register a new patient
+        int patientId = testDataGenerator.registration.createPatient(
+                GenderEnum.MALE,
+                new LocalDate(2000, 9, 1),
+                "John",
+                "Doe");
+
+        // start a new visit
+        int encounterId = testDataGenerator.startVisit(
+                patientId,
+                new LocalDate(2022, 11, 1),
+                VisitTypeEnum.VISIT_TYPE_OPD);
+
+        // enrol patient into hiv program
+        testDataGenerator.program.enrollPatientIntoHIVProgram(
+                patientId,
+                new LocalDate(2022, 11, 3),
+                ConceptEnum.WHO_STAGE_1,
+                TherapeuticLineEnum.FIRST_LINE,
+                new LocalDate(2022, 11, 3));
+
+        // set hiv test date
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCHivTestDate(
+                patientId,
+                new LocalDateTime(2022, 11, 5, 8, 0),
+                new LocalDate(2022, 11, 5),
+                encounterId);
+
+        // set hiv final result
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCFinalResult(
+                patientId,
+                new LocalDateTime(2022, 11, 11, 8, 0),
+                ConceptEnum.POSITIVE,
+                encounterId);
+
+
+        // execute
+        String query = readReportQuery(ReportEnum.CDV_TS_REPORT,
+                "indicator3_CDV_TS_number_of_people_tested_positive_in_the_month_disaggregation_entry_point.sql",
+                new LocalDate(2022, 11, 1),
+                new LocalDate(2022, 11, 30));
+
+        List<Map<String, Object>> result = getReportResult(query);
+
+        // Assert
+        assertEquals(result.get(0).get("Title"),
+                "Number of people tested positive in the month disaggregated by entry point");
+        assertEquals(result.get(0).get("Index"), 0);
+        assertEquals(result.get(0).get("Emergency"), 0);
+        assertEquals(result.get(0).get("CPN / ANC"), 0);
+        assertEquals(result.get(0).get("SA/Maternité / Delivery room/Postpartumn"), 0);
+        assertEquals(result.get(0).get("VCT"), 0);
+        assertEquals(result.get(0).get("Hospitalization"), 0);
+        assertEquals(result.get(0).get("Pediatrics"), 0);
+        assertEquals(result.get(0).get("Other testing at TB Unit"), 0);
+        assertEquals(result.get(0).get("Blood Bank"), 0);
+        assertEquals(result.get(0).get("Screening Campaign"), 0);
+        assertEquals(result.get(0).get("Other entry Points"), 0);
+
+    }
+
+    @Test
+    public void shouldNotCountHivPatientOutsideReportingPeriod() throws Exception {
+        // register a new patient
+        int patientId = testDataGenerator.registration.createPatient(
+                GenderEnum.MALE,
+                new LocalDate(2000, 9, 1),
+                "John",
+                "Doe");
+
+        // start a new visit
+        int encounterId = testDataGenerator.startVisit(
+                patientId,
+                new LocalDate(2022, 11, 1),
+                VisitTypeEnum.VISIT_TYPE_OPD);
+
+        // enrol patient into hiv program
+        testDataGenerator.program.enrollPatientIntoHIVProgram(
+                patientId,
+                new LocalDate(2022, 11, 3),
+                ConceptEnum.WHO_STAGE_1,
+                TherapeuticLineEnum.FIRST_LINE,
+                new LocalDate(2022, 11, 3));
+
+        // set hiv test date
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCHivTestDate(
+                patientId,
+                new LocalDateTime(2022, 11, 5, 8, 0),
+                new LocalDate(2022, 11, 5),
+                encounterId);
+
+        // set hiv final result
+        testDataGenerator.hivTestingAndCounsellingForm.setHTCFinalResult(
+                patientId,
+                new LocalDateTime(2022, 11, 11, 8, 0),
+                ConceptEnum.POSITIVE,
+                encounterId);
+
+        // execute
+        String query = readReportQuery(ReportEnum.CDV_TS_REPORT,
+                "indicator3_CDV_TS_number_of_people_tested_positive_in_the_month_disaggregation_entry_point.sql",
+                new LocalDate(2023, 01, 1),
+                new LocalDate(2023, 01, 31));
+
+        List<Map<String, Object>> result = getReportResult(query);
+
+        // Assert
+        assertEquals(result.get(0).get("Title"),
+                "Number of people tested positive in the month disaggregated by entry point");
+        assertEquals(result.get(0).get("Index"), 0);
+        assertEquals(result.get(0).get("Emergency"), 0);
+        assertEquals(result.get(0).get("CPN / ANC"), 0);
+        assertEquals(result.get(0).get("SA/Maternité / Delivery room/Postpartumn"), 0);
+        assertEquals(result.get(0).get("VCT"), 0);
+        assertEquals(result.get(0).get("Hospitalization"), 0);
+        assertEquals(result.get(0).get("Pediatrics"), 0);
+        assertEquals(result.get(0).get("Other testing at TB Unit"), 0);
+        assertEquals(result.get(0).get("Blood Bank"), 0);
+        assertEquals(result.get(0).get("Screening Campaign"), 0);
+        assertEquals(result.get(0).get("Other entry Points"), 0);
+
+    }
+
 }
