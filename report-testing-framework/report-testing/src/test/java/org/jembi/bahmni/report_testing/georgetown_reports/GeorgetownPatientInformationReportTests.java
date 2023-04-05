9ffa90ee55b111d698e2cb6a172ff5b9382a533e
tmvumbi2 <tresor.mvumbi@jembi.org>
2023-04-05 15:42:29 +0200

    COM-1250 Fix concatenateARTDrugs to include last hour of the day (#628)
    When verifying if the drug dispence date time is between 2 date, the second date needs to include the time component (set to 23:59:59) to include the full day range

--- report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/georgetown_reports/GeorgetownPatientInformationReportTests.java
+++ report-testing-framework/report-testing/src/test/java/org/jembi/bahmni/report_testing/georgetown_reports/GeorgetownPatientInformationReportTests.java
@@ -100,7 +100,7 @@ public class GeorgetownPatientInformationReportTests extends BaseReportTest {
                                 patientId,
                                 encounterIdOpdVisit,
                                 DrugNameEnum.ABC_3TC_60_30MG,
-                                new LocalDateTime(2020, 1, 5, 8, 0, 0),
+                                new LocalDateTime(2020, 1, 5, 8, 30, 0),
                                 1,
                                 DurationUnitEnum.MONTH,
                                 true);
