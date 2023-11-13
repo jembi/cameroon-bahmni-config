8d534dad7e24c1089aa12bc3e0a2e04556727622
tresor <tresorluz21@gmail.com>
2023-11-14 00:01:50 +0200

    Added fixes in the labor and delivery line listing report

--- openmrs/apps/reports/GEORGETOWN_LABOUR_AND_DELIVERY_REPORT/sql/georgetownLabourAndDeliveryReport.sql
+++ openmrs/apps/reports/GEORGETOWN_LABOUR_AND_DELIVERY_REPORT/sql/georgetownLabourAndDeliveryReport.sql
@@ -8,17 +8,17 @@ SELECT
     getPatientPhoneNumber(r.person_b) as "contactTelephone",
     IF(getObsCodedValue(r.person_b, "235ce45c-0b24-40cb-9d49-72de6566f36e") IS NOT NULL, "Yes", "No") as "HIVTestedBeforeDelivery?",
     getPatientBirthdate(r.person_a) as "dateOfBirth",
-    getObsCodedValueInSectionByNames(r.person_b, "d7b99d69-a262-4fdd-b46c-e47919ae13df", "ca285ab0-7714-41f9-a7cd-16711322c808") as "resultOfHIVTestDoneBeforeDelivery",
-    getObsCodedValue(r.person_b, "0da273f6-92ee-4ddb-8a37-5e18cdc35441") as "dateOfHIVTestDuringDelivery",
-    getObsCodedValue(r.person_b, "d475c2f2-d9b4-4a42-95ad-3ed0fb7045e6") as "resultOfHIVTestDuringDelivery",
-    getObsCodedValue(r.person_b, "3f41c8e5-ce72-4395-83a3-61248b0e83bd") as "dateOfARTInitiation",
-    getObsCodedValue(r.person_b, "1c6106f6-f82e-48e3-8965-0618a267c939") as "ARVStatus",
-    getObsCodedValue(r.person_b, "3f41c8e5-ce72-4395-83a3-61248b0e83bd") as "ARVRegimen",
-    getObsCodedValue(r.person_b, "4618c38e-89a8-453e-ba03-45c123e313ce") as "deliveryMode",
-    getObsCodedValue(r.person_b, "37fc1352-009c-4290-b740-265a77f29bbb") as "sexOfNewborn",
-    getObsCodedValue(r.person_b, "d88caa84-c4d7-4001-ba8d-208b71250de7") as "weightOfInfantAtBirth",
-    getObsCodedValue(r.person_b, "8241fe28-aee1-4782-8cff-3faf2e314a68") as "ARVProphylaxis",
-    getObsCodedValue(r.person_b, "aec53b28-e957-492e-8421-244dff836076") as "prophylaxisARVProtocol"
+    getObsCodedValueInSectionByNames(r.person_b, "L&D_Test result", "L&D_Before") as "resultOfHIVTestDoneBeforeDelivery",
+    getObsDatetimeValue(r.person_b, "0da273f6-92ee-4ddb-8a37-5e18cdc35441") as "dateOfHIVTestDuringDelivery",
+    getObsCodedValueInSectionByNames(r.person_b, "L&D_Test result", "HIV testing in the delivery room or postpartum") as "resultOfHIVTestDuringDelivery",
+    getObsDatetimeValue(r.person_b, "d986e715-14fd-4ae1-9ef2-7a60e3a6a54e") as "dateOfARTInitiation",
+    getObsCodedValueInSectionByNames(r.person_b, "L&D_Status", "ART status during pregnancy") as "ARVStatus",
+    getObsCodedValueInSectionByNames(r.person_b, "L&D_Protocol","ART status during pregnancy") as "ARVRegimen",
+    getObsCodedValueInSectionByNames(r.person_b, "Type of delivery","L&D_Delivery") as "deliveryMode",
+    getObsCodedValueInSectionByNames(r.person_b, "Gender", "L&D_Outcome  at birth and care of the newborn") as "sexOfNewborn",
+    getObsCodedValueInSectionByNames(r.person_b, "WEIGHT", "L&D_Outcome  at birth and care of the newborn") as "weightOfInfantAtBirth",
+    getObsCodedValueInSectionByNames(r.person_b, "Prophylaxis ARV","L&D_Outcome  at birth and care of the newborn") as "ARVProphylaxis",
+    getObsCodedValueInSectionByNames(r.person_b, "Prophylaxis ARV Protocol","L&D_Outcome  at birth and care of the newborn") as "prophylaxisARVProtocol"
 FROM (SELECT @a:= 0) AS a, relationship r
                                JOIN relationship_type rt ON rt.relationship_type_id = r.relationship AND rt.a_is_to_b = "RELATIONSHIP_BIO_MOTHER"
                                JOIN patient_identifier pi ON pi.patient_id = r.person_a AND pi.preferred = 1
