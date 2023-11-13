4ed1301a57aeaa50726e652415be8927390fa20c
tresor <tresorluz21@gmail.com>
2023-11-14 01:12:31 +0200

    Added a new fix in the referal line listing report

--- openmrs/apps/reports/GEORGETOWN_REFERRAL_REPORT/sql/georgetownReferralReport.sql
+++ openmrs/apps/reports/GEORGETOWN_REFERRAL_REPORT/sql/georgetownReferralReport.sql
@@ -9,7 +9,7 @@ SELECT
     getPatientPhoneNumber(p.patient_id) as "contactTelephone",
     getHIVTestDate(p.patient_id, "2000-01-01","2100-01-01") as "HIVTestDate",
     getPatientARVStartDate(p.patient_id) as "dateOfARTInitiation",
-    getObsCodedValue(p.patient_id, "c5b20e93-56c8-45e5-b65b-2b42ee49ecb0") as "referralDate",
+    getObsDatetimeValue(p.patient_id, "c5b20e93-56c8-45e5-b65b-2b42ee49ecb0") as "referralDate",
     getObsCodedValue(p.patient_id, "cb654311-dac9-45e0-afc0-3c78ed432ea1") as "referralService",
     getObsCodedValue(p.patient_id, "2c45dfd3-5942-4b92-b440-e7dba02624df") as "whyToTransfer?",
     getObsCodedValue(p.patient_id, "982f8e67-f28c-444a-a739-d9f4051f96fc") as "howUrgent",
