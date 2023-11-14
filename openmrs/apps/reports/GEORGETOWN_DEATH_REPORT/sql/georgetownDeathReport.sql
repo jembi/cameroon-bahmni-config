c27b918bd81b3211f643e7c2e77ae934cd706486
tresor <tresorluz21@gmail.com>
2023-11-14 12:36:42 +0200

    Added extra column in the death line listing report

--- openmrs/apps/reports/GEORGETOWN_DEATH_REPORT/sql/georgetownDeathReport.sql
+++ openmrs/apps/reports/GEORGETOWN_DEATH_REPORT/sql/georgetownDeathReport.sql
@@ -7,7 +7,8 @@ SELECT
     getPatientGender(p.patient_id) as "sex",
     CONCAT(getPatientVillage(p.patient_id),",",getPatientPreciseLocation(p.patient_id)) as "address",
     getPatientPhoneNumber(p.patient_id) as "contactTelephone",
-    getObsCodedValue(p.patient_id, "67d7ed45-8c77-4cce-8bc2-03ea5d15490f") as "dateOfDeathNotification",
+    IF(getProgramAttributeValueWithinReportingPeriod(p.patient_id, "#startDate#", "#endDate#", "242c9027-dc2d-42e6-869e-045e8a8b95cb", "HIV_PROGRAM_KEY")="true","Yes","No") as "PatientIsBreastfeeding",
+    getObsDatetimeValue(p.patient_id, "67d7ed45-8c77-4cce-8bc2-03ea5d15490f") as "dateOfDeathNotification",
     getObsCodedValue(p.patient_id, "6ea73229-7399-41ae-a6e5-68942a354956") as "PlaceOfDeath",
     getObsCodedValue(p.patient_id, "30e1c728-f008-4dd1-ab34-2c34a00fd3a5") as "reasonOfDeath",
     IF(getPatientARVStartDate(p.patient_id) IS NOT NULL, "Yes", "No") as "onART",
@@ -21,4 +22,4 @@ SELECT
     getProgramAttributeValueWithinReportingPeriod(p.patient_id, "#startDate#", "#endDate#", "8bb0bdc0-aaf3-4501-8954-d1b17226075b", "HIV_PROGRAM_KEY") as "apsInCharge"
 FROM patient p, (SELECT @a:= 0) AS a
 WHERE
-    getObsCodedValue(p.patient_id, "67d7ed45-8c77-4cce-8bc2-03ea5d15490f") BETWEEN "#startDate#" AND "#endDate#";
+    getObsDatetimeValue(p.patient_id, "67d7ed45-8c77-4cce-8bc2-03ea5d15490f") BETWEEN "#startDate#" AND "#endDate#";
