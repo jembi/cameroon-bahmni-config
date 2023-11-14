d399366500e2de977ae43300998d6b400fd7f1e6
tresor <tresorluz21@gmail.com>
2023-11-14 10:17:23 +0200

    Fixed the referred by column

--- openmrs/apps/reports/GEORGETOWN_REFERRAL_REPORT/sql/georgetownReferralReport.sql
+++ openmrs/apps/reports/GEORGETOWN_REFERRAL_REPORT/sql/georgetownReferralReport.sql
@@ -13,7 +13,7 @@ SELECT
     getObsCodedValue(p.patient_id, "cb654311-dac9-45e0-afc0-3c78ed432ea1") as "referralService",
     getObsCodedValue(p.patient_id, "2c45dfd3-5942-4b92-b440-e7dba02624df") as "whyToTransfer?",
     getObsCodedValue(p.patient_id, "982f8e67-f28c-444a-a739-d9f4051f96fc") as "howUrgent",
-    getObsCodedValue(p.patient_id, "57a5de55-acf0-4695-bbc2-a2acc1721de7") as "referredBy"
+    getObsTextValue(p.patient_id, "d8a7d14b-74e9-4069-b689-270436134e1d") as "referredBy"
 FROM patient p, (SELECT @a:= 0) AS a
 WHERE
     getObsDatetimeValue(p.patient_id, "c5b20e93-56c8-45e5-b65b-2b42ee49ecb0") BETWEEN "#startDate#" AND "#endDate#";
