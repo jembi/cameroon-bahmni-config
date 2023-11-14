f8a7e6cfce070bd94011c2f5b183ab89176884b1
ASomaroo <124565254+ASomaroo@users.noreply.github.com>
2023-11-15 00:33:34 +0200

    COM-1259: Added line listing reports. (#665)
    * COM-1259: Added line listing reports.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: Actioned code review comments.
    
    * COM-1259: reverting changes.
    
    * Added fixes in the labor and delivery line listing report
    
    * Added a new fix in the referal line listing report
    
    * Fixed the referred by column
    
    * Added additional changes in the birth line listing report
    
    * Added fixes for ARV Dispensation Report
    
    * Added extra column in the death line listing report
    
    * added fixes and new columns in the Patient Outcome line listing report
    
    * Renamed report
    
    * Added new changes for the EAC line listing report
    
    * Updated the condition for the EAC report
    
    * ...
    
    * Added new template files for the new line listing reports
    
    ---------
    
    Co-authored-by: tresor <tresorluz21@gmail.com>

---
+++ openmrs/apps/reports/GEORGETOWN_ARV_DRUG_DISPENSATION_REPORT/sql/georgetownArvDrugDispensationReport.sql
@@ -0,0 +1,32 @@
+SELECT
+    CAST(@a:=@a+1 AS CHAR) as "serialNumber",
+    getPatientARTNumber(p.patient_id) as "ArtCode",
+    getFacilityName() as "facilityName",
+    getPatientIdentifier(p.patient_id) as "uniquePatientID",
+    getPatientAge(p.patient_id) as "ageAtEnrollment",
+    getPatientBirthdate(p.patient_id) as "birthDate",
+    getPatientGender(p.patient_id) as "sex",
+    IF(getProgramAttributeValueWithinReportingPeriod(p.patient_id, "#startDate#", "#endDate#", "242c9027-dc2d-42e6-869e-045e8a8b95cb", "HIV_PROGRAM_KEY")="true","Yes","No") as "patientIsBreastfeeding",
+    getPatientHIVTestDate(p.patient_id) as "HIVTestDate",
+    getPatientARVStartDate(p.patient_id) as "dateOfArtInitiation",
+    CONCAT(getPatientVillage(p.patient_id),",",getPatientPreciseLocation(p.patient_id)) as "address",
+    getPatientPhoneNumber(p.patient_id) as "contactTelephone",
+    getObsNumericValue(p.patient_id,"5089AAAAAAAAAAAAAAAAAAAAAAAAAAAA") as "Weight(Kg)",
+    getObsNumericValue(p.patient_id,"5090AAAAAAAAAAAAAAAAAAAAAAAAAAAA") as "Height(Cm)",
+    getPatientMostRecentProgramTrackingStateValue(p.patient_id,"en","HIV_PROGRAM_KEY") as "clinicalWhoStage",
+    getFirstARVPrescribed(p.patient_id) as "regimentAtArtInitiation",
+    getARVTherapeuticLineAtInitiation(p.patient_id) as "lineAtInitiation",
+    getPatientMostRecentProgramAttributeCodedValue(p.patient_id, "397b7bc7-13ca-4e4e-abc3-bf854904dce3", "en") as "currentLine",
+    getLastARVDispensed(p.patient_id,"2000-01-01", "#endDate#") as "lastARTDispensed",
+    getDurationMostRecentArvTreatmentInDays(p.patient_id,"2000-01-01", "#endDate#") as "numberOfDaysDispensed",
+    getARTAppointmentOnOrAfterDate(p.patient_id, COALESCE(GREATEST("#startDate#", getLastArvPickupDate(p.patient_id, "2000-01-01", "#endDate#")),"#startDate#")) as "lastAppointmentDate",
+    getPatientMostRecentProgramAttributeCodedValue(p.patient_id, "39202f47-a709-11e6-91e9-0800270d80ce", "en") as "reasonForARTDispensation",
+    getPatientMostRecentProgramAttributeCodedValue(p.patient_id, '12afc1d3-74ba-428f-9a77-3fde76a136e4', "en") as "ARTDispensationModel",
+    IF(getObsCodedValue(p.patient_id, "248e21db-98f8-49fc-b596-fe9042b013ac") IS NOT NULL, "True", "False") as "kp",
+    getObsCodedValue(p.patient_id, "248e21db-98f8-49fc-b596-fe9042b013ac") as "kpType",
+    getViralLoadTestDate(p.patient_id) as "lastVLSampleCollectionDate",
+    getViralLoadTestResult(p.patient_id) as "result",
+    getProgramAttributeValueWithinReportingPeriod(p.patient_id, "#startDate#", "#endDate#", "8bb0bdc0-aaf3-4501-8954-d1b17226075b", "HIV_PROGRAM_KEY") as "apsInCharge"
+FROM patient p, (SELECT @a:= 0) AS a
+WHERE
+    patientHasStartedARVTreatmentDuringReportingPeriod(p.patient_id, "#startDate#", "#endDate#");
---
+++ openmrs/apps/reports/GEORGETOWN_PATIENT_OUTCOME_REPORT/sql/georgetownPatientOutcomeReport.sql
@@ -0,0 +1,31 @@
+SELECT
+    CAST(@a:=@a+1 AS CHAR) as "serialNumber",
+    getPatientARTNumber(p.patient_id) as "ArtCode",
+    getFacilityName() as "facilityName",
+    getPatientIdentifier(p.patient_id) as "uniquePatientID",
+    getPatientAge(p.patient_id) as "ageAtEnrollment",
+    getPatientBirthdate(p.patient_id) as "birthDate",
+    getPatientGender(p.patient_id) as "sex",
+    getPatientHIVTestDate(p.patient_id) as "HIVTestDate",
+    getPatientARVStartDate(p.patient_id) as "dateOfArtInitiation",
+    CONCAT(getPatientVillage(p.patient_id),",",getPatientPreciseLocation(p.patient_id)) as "address",
+    getPatientPhoneNumber(p.patient_id) as "contactTelephone",
+    getPatientMostRecentProgramTrackingStateValue(p.patient_id,"en","HIV_PROGRAM_KEY") as "clinicalWhoStage",
+    getFirstARVPrescribed(p.patient_id) as "regimentAtArtInitiation",
+    getARVTherapeuticLineAtInitiation(p.patient_id) as "lineAtInitiation",
+    concatenateARTDrugs(p.patient_id, getMostRecentArvPickupDateBeforeReportEndDate(p.patient_id, "#endDate#"), getMostRecentArvPickupDateBeforeReportEndDate(p.patient_id, "#endDate#")) as "currentRegimen",
+    getPatientMostRecentProgramAttributeCodedValue(p.patient_id, "397b7bc7-13ca-4e4e-abc3-bf854904dce3", "en") as "currentLine",
+    getARTAppointmentOnOrAfterDate(p.patient_id, COALESCE(GREATEST("#startDate#", getLastArvPickupDate(p.patient_id, "2000-01-01", "#endDate#")),"#startDate#")) as "dateOfLastVisit",
+    getDateSecondMostRecentARVAppointmentBeforeOrEqualToDate(p.patient_id, "#endDate#") as "lastAppointmentDate",
+    getPatientMostRecentProgramOutcome(p.patient_id, "en", "HIV_PROGRAM_KEY") as "patientOutcome",
+    getObsDatetimeValue(p.patient_id, "67d7ed45-8c77-4cce-8bc2-03ea5d15490f") as "outcomeDate",
+    IF(getObsCodedValue(p.patient_id, "211f0857-61a3-4049-9777-374c4a592453") IS NOT NULL, "True", "False") as "KP",
+    getObsCodedValue(p.patient_id, "211f0857-61a3-4049-9777-374c4a592453") as "KPType",
+    getObsCodedValue(p.patient_id, "c221869a-3f10-11e4-adec-0800271c1b75") as "profession",
+    getObsTextValue(p.patient_id,"2a535e68-abe9-4e8f-9789-cd51c1a823fc") as "facilityTransferredTo",
+    getObsCodedValue(p.patient_id, "30e1c728-f008-4dd1-ab34-2c34a00fd3a5") as "reasonOfDeath",
+    getPatientMostRecentProgramOutcome(p.patient_id, "en", "HIV_PROGRAM_KEY") as "reasonOfSToppedTreatment",
+    getProgramAttributeValueWithinReportingPeriod(p.patient_id, "#startDate#", "#endDate#", "8bb0bdc0-aaf3-4501-8954-d1b17226075b", "HIV_PROGRAM_KEY") as "apsInCharge"
+FROM patient p, (SELECT @a:= 0) AS a
+WHERE
+    getPatientMostRecentProgramOutcome(p.patient_id, "en", "HIV_PROGRAM_KEY") IS NOT NULL;
