6e5fe2e9348fffaa7a28bfa38ae6cf3a6f908da0
mahaomolise92 <nahaomolise92@gmail.com>
2024-04-11 09:08:45 +0200

    change the report template and rename the the report

--- metadata/reportssql/obs_functions.sql
+++ metadata/reportssql/obs_functions.sql
@@ -86,6 +86,37 @@ BEGIN
 END$$
 DELIMITER ;
 
+-- getObsCodedValuesInSectionByNames for multi-select concepts
+DELIMITER $$
+CREATE FUNCTION getObsCodedValuesInSectionByNames(
+    p_patientId INT(11),
+    p_questionName VARCHAR(250),
+    p_sectionName VARCHAR(250)) RETURNS VARCHAR(1024)
+    DETERMINISTIC
+BEGIN
+    DECLARE result VARCHAR(1024);
+
+    SELECT
+        GROUP_CONCAT(DISTINCT cn_answer.name ORDER BY o.date_created DESC SEPARATOR ', ') INTO result
+    FROM obs o
+        JOIN concept_name cn_question ON o.concept_id = cn_question.concept_id
+        JOIN concept_name cn_answer ON o.value_coded = cn_answer.concept_id        
+    WHERE o.voided = 0
+        AND o.person_id = p_patientId
+        AND cn_question.name = p_questionName
+        AND cn_question.locale='en' AND cn_question.concept_name_type = 'FULLY_SPECIFIED'
+        AND cn_answer.locale='en' AND cn_answer.concept_name_type = 'FULLY_SPECIFIED'
+        AND p_sectionName = (SELECT concept_name.name
+            FROM obs
+                JOIN concept_name ON obs.concept_id = concept_name.concept_id
+            WHERE obs.voided = 0
+                AND obs.obs_id = o.obs_group_id
+                AND concept_name.locale='en' AND concept_name.concept_name_type = 'FULLY_SPECIFIED');
+
+    RETURN (result);
+END$$
+DELIMITER ;
+
 -- getObsCodedShortNameValue
 
 DROP FUNCTION IF EXISTS getObsCodedShortNameValue;
